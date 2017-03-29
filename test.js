ad9bf14fe897629f86eee8726efd3038b87afa30
tigoe <tom.igoe@gmail.com>
2017-03-29 13:48:38 -0400

    updates

--- test.js
+++ test.js
@@ -16,44 +16,50 @@ var sequence = DMX.Animation; // create a new animation sequence instance
 var serialPort = '/dev/cu.usbserial-EN192756';  // your serial port name
 
 // create a new DMX universe on your serial port:
-var universe = dmx.addUniverse('demo', 'enttec-usb-dmx-pro',
+var universe = dmx.addUniverse('mySystem', 'enttec-usb-dmx-pro',
 serialPort);
 
-universe.update({0:0}); // set channel 0 to 0
+var channel = 0;    // channel number
+var level = 0;      // level
+var fadeStep = 1;   // increment to fade; for manual fading
 
+// turn everything off:
+for (channel=0; channel < 256; channel++) {
+  var light = {[channel]: level};      // make an object
+  universe.update(light);              // set channel to 0
+}
 // running a sequence using Animation:
 console.log("running a 12-second animation...");
 var cue = new sequence();
 cue.add({0: 255}, 5000)   // fade channel 0 to 255, 5 seconds
-.delay(2000)          // delay 2 seconds
-.add({0: 0}, 5000)    // fade channel 0 to 0, 5 seconds
-.delay(2000);         // delay 2 seconds
-cue.run(universe, done);  // run the cue, with callback function done
+  .delay(2000)          // delay 2 seconds
+  .add({0: 0}, 5000)    // fade channel 0 to 0, 5 seconds
+  .delay(2000);         // delay 2 seconds
+  cue.run(universe, done);  // run the cue, with callback function done
 
 function done() {
   console.log("done. Now I'll run the loop...");
   setInterval(fade, 20);                // run the fade every 20ms
-
 }
 
 // fading a channel manually:
-var channel = 0;    // channel number
-var level = 1;      // level
-var fadeStep = 1;   // increment to fade
-
 function fade(){
-  var light = {[channel]: level};    // make an object
-  universe.update(light);            // update the light
+  var light = {[channel]: level};      // make an object
+  universe.update(light);              // update the light
 
   // change the level for next time:
-  if (level === 255 || level === 0) { // if 0 or 255
+  if (level === 255 || level === 0) {  // if 0 or 255
     fadeStep = -fadeStep;              // reverse the fade direction
     console.log('loop');
   }
-  level += fadeStep;                  // increment/decrement the fade
+  level += fadeStep;                   // increment/decrement the fade
 }
 
-process.stdin.resume();//so the program will not close instantly
+//----------------------------------------------------
+// this section makes sure the script turns everything off
+// before quitting:
+
+process.stdin.resume();    //make sure the program doesn't close right away
 
 function exitHandler(options, err) {
   if (options.cleanup) console.log('clean');
